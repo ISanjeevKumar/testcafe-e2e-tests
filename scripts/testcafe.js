d13fd9bd0e66c5d3a7c66ff5e7270bddb6ccc74f
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2021-11-21 10:18:15 +0530

    Added xUnit reporter

--- scripts/testcafe.js
+++ scripts/testcafe.js
@@ -1,5 +1,7 @@
 const createTestCafe = require("testcafe");
 const yargs = require('yargs')
+const fse = require('fs-extra')
+const stream = fse.createWriteStream('result.xml');
 
 const argv = yargs.options({
     'browser': {
@@ -20,14 +22,15 @@ const {
     ENVIRONMENT
 } = process.env
 
-async function runTest(browser) {
+async function runTest() {
     const testcafe = await createTestCafe(null, 1337, 1338);
     const runner = await testcafe.createRunner();
     console.log('Environment:', ENVIRONMENT)
     runner
         .src(argv.test)
         .browsers(argv.browser)
-        .reporter('spec')
+        //.reporter('spec')
+        .reporter('xunit', stream)
         .screenshots('test-reports/screenshots', true)
         .run({
             selectorTimeout: 20000,
@@ -38,6 +41,7 @@ async function runTest(browser) {
         .then(failed => {
             console.log(failed + " Tests failed");
             testcafe.close();
+            process.exit(failed);
         });
 };
 
